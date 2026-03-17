bf1df60446af04b4e9891589822eb5ab843a5cc9
Davi Barbosa <dabisilvaond@gmail.com>
2026-03-17 15:50:46 -0300

    rafactoring button variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,11 +9,6 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-blue-200 text-white hover:bg-blue-300',
-        destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline:
-          'border border-gray-400 bg-gray-700 transition-colors duration-200 hover:text-blue-200 hover:border-blue-200',
         primary: 'bg-blue-200 text-white hover:bg-blue-100 rounded-full',
         secondary: 'bg-gray-100 text-gray-800 hover:bg-blue-100 rounded-full',
       },
@@ -25,7 +20,7 @@ const buttonVariants = cva(
       },
     },
     defaultVariants: {
-      variant: 'default',
+      variant: 'primary',
       size: 'default',
     },
   }
