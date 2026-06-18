106c64b9dc6554cb997b35721f76b3fa5a48e333
Davi Barbosa <dabisilvaond@gmail.com>
2026-06-18 15:47:55 -0300

    adding SEO in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,30 @@
 import { LandingPage } from '@/templates/landing-page/landing-page';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Site.Set DS',
+  description: 'Sell your products as an affiliate in one place',
+  robots: {
+    index: true,
+    follow: true,
+  },
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Sell your products as an affiliate in one place',
+    images: {
+      url: '/og-image.jpg',
+      width: 800,
+      height: 600,
+      alt: 'Site.Set DS',
+    },
+    url: 'https://site-blog-five-eta.vercel.app/og-image.jpg',
+    siteName: 'Site.Set DS',
+    type: 'website',
+    locale: 'en_US',
+    alternateLocale: ['pt_BR'],
+    countryName: 'Brazil',
+  },
+};
 
 export default function Home() {
   return <LandingPage />;
