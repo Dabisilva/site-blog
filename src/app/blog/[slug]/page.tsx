a0c4462f204ea2affd765a62335bd020cb23bca8
Davi Barbosa <dabisilvaond@gmail.com>
2026-06-18 16:11:17 -0300

    adding dinamic metadata on post

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { Post } from '@/templates/blog';
 import { allPosts, type Post as PostType } from 'contentlayer/generated';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
 export async function generateStaticParams() {
@@ -13,6 +14,35 @@ type PostPageProps = {
     slug: string;
   }>;
 };
+
+export async function generateMetadata({ params }: PostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug.toLowerCase() === slug?.toLowerCase()) as PostType;
+
+  if (!post) {
+    return {
+      title: 'Post not found',
+      description: 'Post not found',
+    };
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [
+      {
+        name: post.author.name,
+      },
+    ],
+    robots: {
+      index: true,
+      follow: true,
+    },
+    openGraph: {
+      images: [post.image],
+    },
+  };
+}
 
 export default async function PostPage({ params }: PostPageProps) {
   const { slug } = await params;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,34 @@
 import { Layout } from '@/components/layout';
 import { ThemeProvider } from '@/components/theme-provider';
-import { siteConfig, siteUrl } from '@/lib/site-config';
+import { siteConfig } from '@/lib/site-config';
 import '@/styles/globals.css';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  metadataBase: siteUrl,
-  title: {
-    default: siteConfig.name,
-    template: `%s | ${siteConfig.name}`,
-  },
+  title: siteConfig.name,
   description: siteConfig.description,
+  robots: {
+    index: true,
+    follow: true,
+  },
+  openGraph: {
+    title: siteConfig.name,
+    description: siteConfig.description,
+    url: siteConfig.url,
+    siteName: siteConfig.name,
+    type: 'website',
+    locale: siteConfig.locale,
+    alternateLocale: [...siteConfig.alternateLocales],
+    countryName: siteConfig.countryName,
+    images: [
+      {
+        url: '/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set DS',
+      },
+    ],
+  },
 };
 
 const themeScript = `
